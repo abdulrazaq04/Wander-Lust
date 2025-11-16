b3eeedcc34e800767a8e0b31de8cd5ca8d8136f9
abdulrazaq04 <kingabdulrazzaq0402@gmail.com>
2025-11-16 07:27:15 +0530

    Refactor review middleware and apply related updates
    - Updated review model logic in models/review.js
    - Improved middleware and route handling in routes/listing.js
    - Updated review routes in routes/review.js
    - Ensured middleware checks and validation are correctly implemented
    changes made in  the middleware review and implemented

--- routes/review.js
+++ routes/review.js
@@ -3,15 +3,16 @@ const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js"); 
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js"); 
-const {validateReview} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 // post route
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/",isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(req.params.id);
 
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;   
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
@@ -20,7 +21,7 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
 }));
     
 // Delete review route
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId",isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
     const {id, reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
